cd9463cde05644d20fac0af8ff63ab99fd4d989e
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-05-18 22:59:28 +0300

    created a search

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
-import React, { useState } from 'react';
+import React, { useState, useMemo } from 'react';
 import './styles/App.css';
 import PostList from 'components/PostList';
 
-import PostForm from 'components/UI/PostForm';
+import PostForm from 'components/PostForm';
 import MySelect from 'components/UI/select/MySelect';
+import MyInput from 'components/UI/input/MyInput';
 
 export const App = () => {
   const [posts, setPosts] = useState([
@@ -12,21 +13,45 @@ export const App = () => {
     { id: 3, title: 'JavaScript', body: 'Get good at JavaScript' },
   ]);
   const [selectedSort, setSelectedSort] = useState('');
+  const [searchQuery, setSearchQuery] = useState('');
+
+  const sortedPosts = useMemo(() => {
+    // console.log('Сортування');
+    if (selectedSort) {
+      return [...posts].sort((a, b) =>
+        a[selectedSort].localeCompare(b[selectedSort])
+      );
+    }
+    return posts;
+  }, [selectedSort, posts]);
+
+  const sortedAndSearchedPosts = useMemo(() => {
+    return sortedPosts.filter(post =>
+      post.title.toLowerCase().includes(searchQuery)
+    );
+  }, [searchQuery, sortedPosts]);
+
   const createPost = newPost => {
     setPosts([...posts, newPost]);
   };
+
   const removePost = post => {
     setPosts(posts.filter(p => p.id !== post.id));
   };
   const sortPosts = sort => {
     setSelectedSort(sort);
-    setPosts([...posts].sort((a, b) => a[sort].localeCompare(b[sort])));
   };
+
   return (
     <div className="App">
       <PostForm create={createPost} />
       <hr style={{ margin: '15px 0' }} />
       <div>
+        <MyInput
+          placeholder="Пошук ..."
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+        />
         <MySelect
           value={selectedSort}
           onChange={sortPosts}
@@ -37,8 +62,12 @@ export const App = () => {
           ]}
         />
       </div>
-      {posts.length ? (
-        <PostList remove={removePost} posts={posts} title="Список постів" />
+      {sortedAndSearchedPosts.length ? (
+        <PostList
+          remove={removePost}
+          posts={sortedAndSearchedPosts}
+          title="Список постів"
+        />
       ) : (
         <h1 style={{ textAlign: 'center' }}>Пости не знайдені</h1>
       )}
