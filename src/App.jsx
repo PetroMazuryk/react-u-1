729a96ba6540d002ab9e2d92dea3844fbdfab3ae
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-05-18 04:27:47 +0300

    add file

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import './styles/App.css';
+import PostItem from 'components/PostItem';
+
+export const App = () => {
+  const [posts] = useState([
+    { id: 1, title: 'JavaScript', body: 'Description' },
+    { id: 2, title: 'JavaScript 2', body: 'Description' },
+    { id: 3, title: 'JavaScript 3', body: 'Description' },
+  ]);
+  return (
+    <div className="App">
+      {posts.map(post => (
+        <PostItem post={post} key={post.id} />
+      ))}
+    </div>
+  );
+};
+
+export default App;
