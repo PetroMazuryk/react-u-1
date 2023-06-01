56d69c43184a3c114b9c065b0413b84428c26f73
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2023-06-01 13:36:20 +0300

    add basename

--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -1,7 +1,11 @@
 import React from 'react';
 import MyButton from './UI/button/MyButton';
+// import { useHistory } from 'react-router-dom';
+
 const PostItem = props => {
   //   console.log(props);
+  // const router = useHistory();
+  // console.log(router);
   return (
     <div className="post">
       <div className="post__content">
@@ -12,6 +16,7 @@ const PostItem = props => {
         <div>{props.post.body}</div>
       </div>
       <div className="post__btn">
+        <MyButton onClick={() => props.remove(props.post)}>Відкрити</MyButton>
         <MyButton onClick={() => props.remove(props.post)}>Видалити</MyButton>
       </div>
     </div>
