376297a34aa45f44a5b9916fc9717ae110d4a02a
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-06-28 18:03:50 +0300

    replace href with to

--- src/components/SocialIcons/SocialIcons.jsx
+++ src/components/SocialIcons/SocialIcons.jsx
@@ -14,22 +14,22 @@ const SocialIcons = () => {
   return (
     <ul className={styles.socialList}>
       <li className={styles.socialItem}>
-        <Link href="#" className={styles.socialLink} aria-label="Facebook">
+        <Link to="#" className={styles.socialLink} aria-label="Facebook">
           <FaFacebookF className={styles.icon} />
         </Link>
       </li>
       <li className={styles.socialItem}>
-        <Link href="#" className={styles.socialLink} aria-label="Twitter">
+        <Link to="#" className={styles.socialLink} aria-label="Twitter">
           <FaTwitter className={styles.icon} />
         </Link>
       </li>
       <li className={styles.socialItem}>
-        <Link href="#" className={styles.socialLink} aria-label="LinkedIn">
+        <Link to="#" className={styles.socialLink} aria-label="LinkedIn">
           <FaLinkedinIn className={styles.icon} />
         </Link>
       </li>
       <li className={styles.socialItem}>
-        <Link href="#" className={styles.socialLink} aria-label="Google Plus">
+        <Link to="#" className={styles.socialLink} aria-label="Google Plus">
           <FaGooglePlusG className={styles.icon} />
         </Link>
       </li>
