8640ecea0f88e95b32585640fb2a30ee3f66b238
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-07-03 09:19:43 +0300

    add socialLinks array

--- src/components/SocialIcons/SocialIcons.jsx
+++ src/components/SocialIcons/SocialIcons.jsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import {
   FaFacebookF,
   FaTwitter,
@@ -8,48 +7,70 @@ import {
   FaYoutube,
   FaTelegramPlane,
   FaInstagram,
+  FaWhatsapp,
 } from 'react-icons/fa';
 
 import styles from './SocialIcons.module.css';
 
+const socialLinks = [
+  {
+    href: 'https://facebook.com',
+    label: 'Facebook',
+    icon: <FaFacebookF className={styles.icon} />,
+  },
+  {
+    href: 'https://twitter.com',
+    label: 'Twitter',
+    icon: <FaTwitter className={styles.icon} />,
+  },
+  {
+    href: 'https://linkedin.com',
+    label: 'LinkedIn',
+    icon: <FaLinkedinIn className={styles.icon} />,
+  },
+  {
+    href: 'https://plus.google.com',
+    label: 'Google Plus',
+    icon: <FaGooglePlusG className={styles.icon} />,
+  },
+  {
+    href: 'https://youtube.com',
+    label: 'YouTube',
+    icon: <FaYoutube className={styles.icon} />,
+  },
+  {
+    href: 'https://telegram.org',
+    label: 'Telegram',
+    icon: <FaTelegramPlane className={styles.icon} />,
+  },
+  {
+    href: 'https://instagram.com',
+    label: 'Instagram',
+    icon: <FaInstagram className={styles.icon} />,
+  },
+  {
+    href: 'https://wa.me/1234567890',
+    label: 'WhatsApp',
+    icon: <FaWhatsapp className={styles.icon} />,
+  },
+];
+
 const SocialIcons = () => {
   return (
     <ul className={styles.socialList}>
-      <li className={styles.socialItem}>
-        <Link to="#" className={styles.socialLink} aria-label="Facebook">
-          <FaFacebookF className={styles.icon} />
-        </Link>
-      </li>
-      <li className={styles.socialItem}>
-        <Link to="#" className={styles.socialLink} aria-label="Twitter">
-          <FaTwitter className={styles.icon} />
-        </Link>
-      </li>
-      <li className={styles.socialItem}>
-        <Link to="#" className={styles.socialLink} aria-label="LinkedIn">
-          <FaLinkedinIn className={styles.icon} />
-        </Link>
-      </li>
-      <li className={styles.socialItem}>
-        <Link to="#" className={styles.socialLink} aria-label="Google Plus">
-          <FaGooglePlusG className={styles.icon} />
-        </Link>
-      </li>
-      <li className={styles.socialItem}>
-        <Link to="#" className={styles.socialLink} aria-label="YouTube">
-          <FaYoutube className={styles.icon} />
-        </Link>
-      </li>
-      <li className={styles.socialItem}>
-        <Link to="#" className={styles.socialLink} aria-label="Telegram">
-          <FaTelegramPlane className={styles.icon} />
-        </Link>
-      </li>
-      <li className={styles.socialItem}>
-        <Link to="#" className={styles.socialLink} aria-label="Instagram">
-          <FaInstagram className={styles.icon} />
-        </Link>
-      </li>
+      {socialLinks.map(({ href, label, icon }) => (
+        <li className={styles.socialItem} key={label}>
+          <a
+            href={href}
+            className={styles.socialLink}
+            aria-label={label}
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {icon}
+          </a>
+        </li>
+      ))}
     </ul>
   );
 };
