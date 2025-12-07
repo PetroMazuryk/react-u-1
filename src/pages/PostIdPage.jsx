34c1a8612446ab93b10bd747d80a06aa61223529
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-12-07 14:17:11 +0200

    using the useFetching hook in the PostIdPage component

--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,35 +1,28 @@
 import {useEffect, useState} from "react";
 import {useParams} from "react-router-dom";
 import PostService from "../API/PostService";
+import {useFetching} from "../hooks/useFetching";
 
 const PostIdPage = () => {
   const {id} = useParams();
   const [post, setPost] = useState(null);
-  const [loading, setLoading] = useState(true);
-  const [error, setError] = useState("");
+
+  const [fetching, isLoading, error] = useFetching(async (id) => {
+    const response = await PostService.getById(id);
+    setPost(response.data);
+  });
 
   useEffect(() => {
-    const fetchPost = async () => {
-      try {
-        const response = await PostService.getById(id);
-        setPost(response.data);
-      } catch (e) {
-        setError("Не вдалося завантажити пост");
-      } finally {
-        setLoading(false);
-      }
-    };
+    fetching(id);
+  }, [id, fetching]);
 
-    fetchPost();
-  }, [id]);
-
-  if (loading) return <div>Завантаження...</div>;
+  if (isLoading) return <div>Завантаження...</div>;
   if (error) return <div>{error}</div>;
 
   return (
     <>
-      <h1>{post.title}</h1>
-      <p>{post.body}</p>
+      <h1>{post?.title}</h1>
+      <p>{post?.body}</p>
     </>
   );
 };
