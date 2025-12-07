51ee2f9a0d204c439044c9b6549a0d68dbdbb4fd
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-12-07 14:21:01 +0200

    clear error state before each fetch request

--- src/hooks/useFetching.js
+++ src/hooks/useFetching.js
@@ -1,21 +1,23 @@
-import { useState, useCallback } from 'react';
+import {useState, useCallback} from "react";
 
-export const useFetching = callback => {
+export const useFetching = (callback) => {
   const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
 
   const fetching = useCallback(
     async (...args) => {
       try {
+        setError("");
         setIsLoading(true);
+
         await callback(...args);
       } catch (e) {
-        setError(e.message);
+        setError(e.message || "Сталася помилка");
       } finally {
         setIsLoading(false);
       }
     },
-    [callback]
+    [callback],
   );
 
   return [fetching, isLoading, error];
