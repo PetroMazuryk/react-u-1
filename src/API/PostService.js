38f104df7dae492b931026e9b9222c583189cd8a
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-12-09 18:25:29 +0200

    show email and comment body

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -1,22 +1,29 @@
-import axios from 'axios';
+import axios from "axios";
 
 export default class PostService {
   static async getAll(limit = 10, page = 1) {
     const response = await axios.get(
-      'https://jsonplaceholder.typicode.com/posts',
+      "https://jsonplaceholder.typicode.com/posts",
       {
         params: {
           _limit: limit,
           _page: page,
         },
-      }
+      },
     );
     return response;
   }
 
   static async getById(id) {
     const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/posts/${id}`
+      `https://jsonplaceholder.typicode.com/posts/${id}`,
+    );
+    return response;
+  }
+
+  static async getCommentsBypostId(id) {
+    const response = await axios.get(
+      `https://jsonplaceholder.typicode.com/posts/${id}/comments`,
     );
     return response;
   }
--- src/pages/PostIdPage.jsx
+++ src/pages/PostIdPage.jsx
@@ -1,30 +1,55 @@
+import React from "react";
 import {useEffect, useState, useCallback} from "react";
 import {useParams} from "react-router-dom";
 import PostService from "../API/PostService";
 import {useFetching} from "../hooks/useFetching";
+import {Loader} from "lucide-react";
 
 const PostIdPage = () => {
   const {id} = useParams();
   const [post, setPost] = useState(null);
+  const [comments, setComments] = useState([]);
 
   const fetchPostById = useCallback(async (id) => {
     const response = await PostService.getById(id);
     setPost(response.data);
   }, []);
+  const fetchPostByIdComments = useCallback(async (id) => {
+    const response = await PostService.getCommentsBypostId(id);
+    setComments(response.data);
+  }, []);
 
   const [fetching, isLoading, error] = useFetching(fetchPostById);
+  const [fetchComment, isCommentLoading, commentError] = useFetching(
+    fetchPostByIdComments,
+  );
 
   useEffect(() => {
     fetching(id);
-  }, [id, fetching]);
+    fetchComment(id);
+  }, [id, fetching, fetchComment]);
 
   if (isLoading) return <div>Завантаження...</div>;
   if (error) return <div>{error}</div>;
+  if (commentError) return <div>{commentError}</div>;
 
   return (
     <>
       <h1>{post?.title}</h1>
       <p>{post?.body}</p>
+      <h2>Коментарі</h2>
+      {isCommentLoading ? (
+        <Loader />
+      ) : (
+        <div>
+          {comments.map((comm) => (
+            <React.Fragment key={comm.id}>
+              <h5>{comm.email}</h5>
+              <p>{comm.body}</p>
+            </React.Fragment>
+          ))}
+        </div>
+      )}
     </>
   );
 };
