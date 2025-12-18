0633b1f2c0e82c5a60003bf93cb79d3067d558bf
PetroMazuryk <109526725+PetroMazuryk@users.noreply.github.com>
2025-12-18 23:33:12 +0200

    fix get request

--- src/API/PostService.js
+++ src/API/PostService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default class PostService {
   static async getAll(limit = 10, page = 1) {
     const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/pos",
+      "https://jsonplaceholder.typicode.com/posts",
       {
         params: {
           _limit: limit,
